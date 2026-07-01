0e9f00ad37eae7e2faba0b8fb407dd2c5964062e
Wyatt Evans <evanswp4s@gmail.com>
2026-07-01 16:43:43 -0400

    resume update

--- src/commands/resume.ts
+++ src/commands/resume.ts
@@ -1,4 +1,4 @@
-const resumeUrl = "https://drive.google.com/file/d/1SwFWOdl5X5-GCUFhbjdeafMSi5vrQt7w/view?usp=sharing"; // or GitHub Pages path
+const resumeUrl = "https://drive.google.com/file/d/1UXvD41loVFVqliMt5vx7-TE19yMcbopV/view?usp=sharing"; // or GitHub Pages path
 
 export const createResume = (): string[] => {
   const resume: string[] = [];
